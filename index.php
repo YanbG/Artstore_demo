353a82bad7abde0a4f7723026b094a88885d21e6
Yanbing Gong <yanbin@live.unc.edu>
2022-03-12 17:24:16 -0500

    db connection test

--- index.php
+++ index.php
@@ -13,13 +13,18 @@
 
     //connect to DB
     $conn=mysqli_connect($cleardb_server, $cleardb_username,$cleardb_password, $cleardb_db);
-
+    mysqli_select_db($conn, "artworks");
+    if($conn ->connect_error){
+        die("connection failed:".$conn->error);
+    }else{
+        echo"connected successfully";
+    }
 ?>
 
 <?php
 
 $findRecentSQL = "SELECT * FROM artworks ORDER BY timeReleased DESC LIMIT 3";
-$find = mysqli_query($_mysqli, $findRecentSQL);
+$find = mysqli_query($conn, $findRecentSQL);
 for($i = 0; $i < 1; $i++){
     $each = mysqli_fetch_assoc($find);
     $recentID[] = $each['artworkID'];
